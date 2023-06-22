73cd3cdbc9c7dc75b60305889eb74aeec2b2197a
Xyroinee-ID <134149217+Xyroinee-ID@users.noreply.github.com>
2023-06-22 19:21:55 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,47 @@
+<p align="center">
+    <img src="./media/fotonya.jpg" width="100%" style="margin-left: auto;margin-right: auto;display: block;">
+</p>
+<h1 align="center">Clara - MD</h1>
+
+
+## Baca Sebelum Pakai!!
+- Ubah Semua Informasi Owner Di Settings.js
+- Jika Kamu Mengalami Masalah, Bisa Buat Issues Di Page Ini
+- Sc Ini Belum Selesai Sepenuhnya, Pastikan Selalu Pantau :v
+- Untuk Node_Module Kalian Bisa Download Dibawah Ini
+- https://www.mediafire.com/file/f52nygbxa8kb580/node_modules.zip/file
+
+### Sedikit Tentang Bot
+- ✔️ | **Simple** 
+- ✔️ | **No Button** 
+- ✔️ | **Multi Device** 
+---------
+### Fitur Yang Dimiliki
+- ✔️ | Anime 
+- ✔️ | Tools 
+- ✔️ | Remini
+- ✔️ | Quotes
+- ✔️ | Confess
+- ✔️ | Chat Gpt
+- ✔️ | To Anime
+- ✔️ | And Others
+---------
+
+
+##
+[![Owner](https://img.shields.io/badge/Owner%20BOT-25D366?style=for-the-badge&logo=whatsapp&logoColor=white)](https://wa.me/6285760451683) 
+[![Bot](https://img.shields.io/badge/Bot%20Whatsapp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white)](https://wa.me/62857604516837)
+
+
+## Thanks To
+```bash
+𝐀𝐮𝐭𝐡𝐨𝐫 : Xyroinee
+Supported By Vynaa Chan
+Thanks To : 
+- Adiwajshing (Author Baileys)
+- DikaArdnt (Base Script)
+- KizakixD
+- DGXeon
+- KirBotz
+- Ekuzika
+- Dan Semua Yang  Berkontribusi Dalam Pengambangan Script Ini 
---
+++ settings.js
@@ -0,0 +1,80 @@
+const chalk = require("chalk")
+const fs = require("fs")
+
+// Apikey 
+global.apikeys = 'Apikeylu'
+// Note: Kalau Lu Gada Apikeynya Daftar Dulu Biar Semua Fiturnya Aktif, Webnya Di Bawah Ya
+// • https://api.xyroinee.xyz
+
+// Info Owner
+global.owner = ['6285760451683']
+global.nomorown = "6285760451683"
+global.ownername = "Xyroınee-ID."
+global.ownerNumber = ["6285760451683@s.whatsapp.net"]
+global.creator = "6285760451683@s.whatsapp.net"
+global.author = "Clara - MD"
+global.location = "Indonesia"
+
+//Bot Setting
+global.botname = "Clara - MD"
+global.wm = "Follow Instagram: danilelistz02"
+global.packname = "Sticker By"
+global.prefa = ['','!','.','#','&','`']
+global.hituet = 0
+
+// Social Media
+global.yt = "https://www.youtube.com/@Xyroinee"
+global.gh = "https://github.com/Xyroinee"
+global.ig = "https://instagram.com/danilelistz02"
+global.fb = "https://facebook.com/danilelistz19"
+global.web = "https://api.xyroinee.xyz"
+global.gcwa = "https://chat.whatsapp.com/LAWdMIQri9SKee5kqmISH0"
+
+// Foto Bot
+global.thumb = fs.readFileSync("./media/clara.jpg")
+
+// Message/Pesan
+global.mess = {
+success: '_Sukses...!_',
+admin: '_Maaf Kak, Tapi Fitur Ini Hanya Bisa Digunakan Oleh Admin Grup_',
+botAdmin: '_Jadikan Saya Sebagai Admin Terlebih Dahulu Agar Dapan Mengaktifkan Fitur Ini_',
+prem: '_Fitur Ini Khusus Premium Kak, Jika Ingin Menjadi User Premium Ketik .premium Atau Hubungin Owner_',
+owner: '_Maaf Kak, Tapi Fitur Ini Hanya Bisa Digunakan Oleh Ownerku_',
+group: '_Fitur Ini Hanya Bisa Di Pakai Di Dalam Grup_',
+private: '_Fitur Ini Hanya Bisa Di Pakai Di Dalam Chat Pribadi_',
+bot: 'Fitur Khusus Bot',
+wait: '_Sedang Di Proses, Mohon Tunggu_...',
+link: '_Uhm...Linknya Mana?_'
+}
+// Ini Panel Ya Njing
+global.host = "https://zrstore.panelrun.xyz"
+global.application = {
+    api_key: "ptla_ktHU0heRiJ505HeME0OUDR8gAn8rCqnUopxO6CqPq1D", //Ambil Di Panel Lu Caranya Pergi Ke Setting Admin Terus Tap Garis Tiga Pojok Kiri Atas Terus Tap Application API Tinggal Salin Apikeynya
+    c_api_key: "ptlc_GGuRaZMzLoHZaW7AcsjtqcplmOZpr0GQmKYXo2BKOrS" //Ambil Di Panel Lu Caranya Ke Account Setting Atau Tap Foto Akun Lu Terus Tap API Credentials Terus Isi Descriptionya Sange Terus Tap Create Nah Lu Salin C Apikeynya
+}
+global.serverCreate = {
+    nestId: "5", //Nest Id Panel Lu
+    eggId: "15", //Egg Id Panel Lu
+    limits: {
+      db: "1", //Mending Ga Usah Diganti
+      backups: "1", //Mending Ga Usah Diganti
+      allocation: "0" //Mending Ga Usah Diganti
+    },
+    eggs: {
+      environment: {
+        "CUSTOM_COMMAND": "npm start", //Sesuaiin Sama Eggs Panel Lu
+        "GIT_ADDRESS": "",
+        "BRANCH": "",
+        "USERNAME": "",
+        "ACCESS_TOKEN": ""
+      }
+    }
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
